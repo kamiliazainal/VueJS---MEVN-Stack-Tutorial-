08ed492aaa7682b4c89758ff5ba6aa69d5a892e2
unknown <kamiliazainal@gmail.com>
2019-10-09 09:32:05 +0800

    successfully created

---
+++ mevnexample/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="app">
+      <div class="container">
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">
+                Home
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/create" class="nav-link">
+                Create Post
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/posts" class="nav-link">
+                Posts
+              </router-link>
+            </li>
+          </ul>
+        </nav><br>
+        <transition name="fade">
+          <router-view></router-view>
+        </transition>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  // name: 'app'
+}
+</script>
+
+<style>
+    .fade-enter-active, .fade-leave-active {
+      transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-active {
+      opacity: 0
+    }
+</style>
